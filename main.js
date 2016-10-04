cc6cfa9cd5a14ddb93c245e7a679f2fb9a728929
Daniel Mocan <daniel.mocan@fortech.ro>
2016-10-04 15:52:11 +0300

    Added Support for SASS

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './components/routes';
 import { Router, browserHistory } from 'react-router';
+require("!style!css!sass!./style.scss");
+
 
 ReactDOM.render( <Router history={browserHistory} routes={routes} />, document.getElementById('app') );
